cb264a9c344aa7f71fe08573823b511459084a05
Reda El Khattabi <reda.ea@gmail.com>
2015-02-09 14:30:23 -0500

    include outputs

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 wget --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" 'http://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900' -O webfont.tmp
 
-grep url.http webfont.tmp | sed 's/^.*url(//g' | sed 's/).*//g' | xargs wget
+grep url.http webfont.tmp | sed 's/^.*url(//g' | sed 's/).*//g' | xargs wget -P fonts
 
-sed 's,http.*/,,g' webfont.tmp > raleway.css
+sed 's,http.*/,fonts/,g' webfont.tmp > raleway.css
 
